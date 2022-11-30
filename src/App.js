4ce9590d410c51165c316a1305ba37e6a77f68cb
Cadnaan Afrah <cadnaan.afrah@deltacapita.com>
2022-11-30 09:29:23 +0000

    setup burger menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import BurgerMenu from "./components/BurgerMenu";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BurgerMenu />
+    </div>
+  );
 }
 
 export default App;
